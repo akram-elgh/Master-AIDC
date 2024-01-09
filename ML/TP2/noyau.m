64d36247540ae028292f8029f87d943dbadd4087
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2024-01-09 17:04:06 +0100

    Co-authored-by: MAROUAN MORAKIB <MAROUAN-MORAKIB@users.noreply.github.com> Co-authored-by: HoussamKanzallah <HoussamKanzallah@users.noreply.github.com> Co-authored-by: medbakaaa <medbakaaa@users.noreply.github.com> Implementation des tous les noyaux pour tous les fonctions

--- ML/TP2/noyau.m
+++ ML/TP2/noyau.m
@@ -7,14 +7,14 @@ function noyau = noyau(x, x1, choix)
           y = x' * x1 + c;
       case 2
           % Noyau polynomial
-          alpha = 1; % Valeur arbitraire pour alpha, vous pouvez ajuster selon vos besoins
-          c = 0.01; % Valeur arbitraire pour c, vous pouvez ajuster selon vos besoins
+          alpha = 1.5; % Valeur arbitraire pour alpha, vous pouvez ajuster selon vos besoins
+          c = 0.05; % Valeur arbitraire pour c, vous pouvez ajuster selon vos besoins
           d = 1.2; % Valeur arbitraire pour d, vous pouvez ajuster selon vos besoins
-          y = (alpha .* (x' .* x1) + c) .^ d;
+          y = (alpha * (x' * x1) + c) .^ d;
       case 3
           % Noyau gaussien
-        %   sigma = 6.7; 
-          sigma = sqrt(var(x));
+          sigma = 0.2; 
+          % sigma = sqrt(var(x));
           y = exp( -norm(x - x1) .^ 2 ./ (2 * sigma ^ 2));
       case 4
           % Noyau exponentiel
@@ -28,8 +28,12 @@ function noyau = noyau(x, x1, choix)
           y = exp(-norm(x - x1) / sigma);
       case 6
           % Noyau sinc
-          sigma = 5; 
-          y = prod(sin(sigma * (x - x1)) ./ (sigma * (x - x1)));
+          sigma = pi; 
+          if x == x1
+            y = 1;
+          else  
+            y = prod(sin(sigma * (x - x1)) / (x - x1));
+          end  
       otherwise
           error('Type de noyau non reconnu.');
   end
